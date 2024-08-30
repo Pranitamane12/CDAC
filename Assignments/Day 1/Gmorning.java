12b54f2480d3c579b541b7a4b94a9e4aaee8853e
Pranita Mane <167905084+Pranitamane12@users.noreply.github.com>
2024-08-30 15:09:15 +0530

    Delete Assignments directory

--- Assignments/Day 1/Gmorning.java
+++
@@ -1,9 +0,0 @@
-public class Gmorning {
-    public static void main(String[] args) {
-        int time = 4;
-        if(time>5 &&time <12){
-            System.out.println("Good Morning");
-        }
-    }
-    
-}
