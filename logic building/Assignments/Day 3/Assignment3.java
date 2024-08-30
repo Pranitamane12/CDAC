08ed7021635a1054d194d49c6eea0dd9eb88837e
Pranita Mane <167905084+Pranitamane12@users.noreply.github.com>
2024-08-30 15:08:12 +0530

    Add files via upload

---
+++ logic building/Assignments/Day 3/Assignment3.java
@@ -0,0 +1,122 @@
+
+
+ //public class Assignment3{
+	//public static void main(String args[]){
+		
+		//Snippet1
+	/*for (int i= 0 ;i<10;i--){
+		System.out.println(i);
+	}*/
+	//the infine loop created because of decrement operator.
+	//If we add i++ insteaed of i-- then loop will control.
+
+
+      // Snippet2
+	   /*int count = 5; 
+          while (count=0 ) { 
+          System.out.println(count); 
+          count++;
+		  } */
+  // }
+   ///Incompatible error is occured int cannot converted boolean.
+   
+   //Snippet 3
+	 /* int num = 0; 
+      do { 
+          System.out.println(num); 
+           num++; 
+         } while (num > 5);*/
+	 //The loop occured infinite times, when we change while condition it gets coorect output.
+
+
+    //Sinnept 4
+       /*for (int i = 1; i <= 10; i++) 
+	   { 
+           System.out.println(i); 	
+	  
+	   }*/
+      ///We got output as 1 to 10 but we have expect from 1 to 9 so we have to do only i < 10 insteaed equal to .
+	  
+	 // Snippet 5
+         /*for (int i = 10; i>=10; i--){
+	   
+         System.out.println(i);
+	   }*/
+      ////  from 10	to infinite no. gets
+	  
+	  
+	  // Sinnept 6
+	  
+	   //public class Assignment3 { 
+          //public static void main(String[] args){
+		 /*{ 
+		 for(int i = 0; i< 5; i++){
+		 System.out.println(i);
+		 }
+			 System.out.println("Done");
+		  */ 
+		 
+	   
+	   ///Because for loop curly braces is close to program terminated.
+	   
+	   ///Snippet 7
+	   
+         /*int count = 0;
+		 while (count< 10){
+		  System.out.println(count);
+		  count++;
+		 }*/
+		 ///Count variable is not initialize so compile error comes
+		 /// so output will from 0 to 9
+		 
+		 /// Snippetpt 8
+		 
+		/*public class Assignment3 {
+			public static void main(String[]args){
+				int num = 1;
+				do{
+					System.out.println(num);*/
+					//////
+					
+		///Snippet9		
+	public class Assignment3{
+
+    public static void main(String[]args){
+    /*{ 
+	    for (int i=0;i<5;i+=2){
+			System.out.println(i);
+        
+        }*/
+		///no error 
+		///output is 0 2 4
+		
+		///Snippet10
+		/*int num = 10 ;
+		while( num >0){
+			System.out.println(num);
+			num--;
+		}*/
+		///In this while loop is for condition then here num assign value so compile error 
+		  /// get so we give condition num> 0 so print 10 to 1.
+		  
+		//Snippet 11
+         /*int i = 0;
+         while(i<5){
+         System.out.println(i);
+          i+=2;
+		 }		
+          No error , the output is 0 2 4*/	
+
+        //Snippet12
+		int x = 0;
+         for (int i=0;i<5;i++){
+          x = i*2;
+		 }
+          System.out.println(x);		 
+    }
+}				
+		 
+		
+		 
+	 		 
+  
---
+++ logic building/Assignments/Day 3/Loops.java
@@ -0,0 +1,30 @@
+public class Loops{
+	public static void main(String[]args){
+		//int i = 0;
+		/*for(int i=0,j=0;i<5;i++){
+			System.out.println("Hp");
+		}*/
+		
+		/*for(int i = 0;i<5;i++)
+		{
+			for(int j=0;j<5;j++)
+			{
+				System.out.println("Hp");
+			}
+			System.out.println("SP");
+	}*/
+	
+	//int i = 0;
+	//for(System.out.println("Incre,Decreloops");i<5;i++));
+	    // System.out.println("inside for");
+		
+	for (int i = 0 ;  i<9; i++){
+			System.out.println("Inside for loop");
+		}
+		for (int i = 0 ; i<6 ; i++){
+			System.out.println("Inside for loop");	
+		}
+	
+}
+}
+	
