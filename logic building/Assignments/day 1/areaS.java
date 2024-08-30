b57f66de1d58f402b3518ca83be98980c2dfecd7
Pranita Mane <167905084+Pranitamane12@users.noreply.github.com>
2024-08-30 15:04:23 +0530

    Add files via upload

---
+++ logic building/Assignments/day 1/areaS.java
@@ -0,0 +1,11 @@
+
+public class areaC {
+    public static void main(String[] args) {
+        int side = 20;
+        
+        int area;
+        area = side* side;
+        System.out.println("Area of circle:"+area);
+    }
+    
+}
---
+++ logic building/Assignments/day 1/positive.java
@@ -0,0 +1,17 @@
+
+public class positive{
+    public static void main(String[] args) {
+        int num  = -69 ;
+        
+       if( num > 0){
+        System.out.println("n is positive");
+        
+       }
+       else {
+        System.out.println("n is negative");
+       }
+        
+        
+    }
+
+}
