a495b9a00b768886b0b01f8f725997c353a01461
Pranita Mane <167905084+Pranitamane12@users.noreply.github.com>
2024-09-04 23:19:05 +0530

    Update Gmorning.java

--- logic building/Assignments/day 1/Gmorning.java
+++ logic building/Assignments/day 1/Gmorning.java
@@ -3,6 +3,8 @@ public class Gmorning {
         int time = 4;
         if(time>5 &&time <12){
             System.out.println("Good Morning");
+        else{
+            System.out.println("inavalid input");
         }
     }
     
